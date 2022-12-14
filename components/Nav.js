4621b8ee93a8ab3efbb4faa5bae1bfbc88b0f05c
sharan-cell <79302315+sharan-cell@users.noreply.github.com>
2022-12-14 22:16:39 +0530

    Update Nav.js

--- components/Nav.js
+++ components/Nav.js
@@ -12,7 +12,7 @@ const router = useRouter();
 
     <div  className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide  snap-mandatory snap-x scroll-smooth hover:scroll-auto  "  >
     {Object.entries(requests).map(([key, {title, url}]) => 
-       ( <h2 key={key} onClick={() => router.push(`/?genre=${key}`)} className={`font-serif flex-none select-none text-base m-2 last:pr-24 cursor-pointer transition snap-centre duration-100 hover:scale-110 hover:text-white active:text-red-500 ${
+       ( <h2 key={key} onClick={() => router.push(`/?genre=${key}`)} className={`font-sans font-semibold  flex-none select-none text-lg m-2 last:pr-24 cursor-pointer transition snap-centre duration-100 hover:scale-110 hover:text-white active:text-red-500 ${
                   router.asPath === title
                     ? "text-primary-500"
                     : "text-secondary-300"
